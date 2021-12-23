248b004b62d5e12ee65344a20bb333057b5031c5
Aranaktu <xAranaktu@pm.me>
2021-12-23 12:19:29 +0100

    offsets v1.0.1.3

--- CS2D_OVERLAY/CS2D_OVERLAY/headers/SDKGen/offsets.h
+++ CS2D_OVERLAY/CS2D_OVERLAY/headers/SDKGen/offsets.h
@@ -1,25 +1,25 @@
 #pragma once
 
 struct GameOffsets {
-    DWORD pInvalid = 0x3C7BC0;   // EmptyPtr
-    DWORD scale = 0x497A24;
+    DWORD pInvalid = 0x3CEBC0;   // EmptyPtr
 
-    DWORD g_pItemsEntityList = 0x48B7A8;
-    DWORD g_pProjectileFlyingEntityList = 0x48BBB0;
+    DWORD scale = 0x4AAAB4;
 
-    DWORD g_pProjectileGroundEntityList = 0x48BBB4;
+    DWORD g_pItemsEntityList = 0x49E804;
+    DWORD g_pProjectileFlyingEntityList = 0x49EC0C;
+    DWORD g_pProjectileGroundEntityList = g_pProjectileFlyingEntityList + 0x4;
 
-    DWORD g_pPlayersEntityList = 0x487330;
-    DWORD screen_width = 0x497A40;
-    DWORD cursor_x = 0x497CB4;
-    DWORD isMatchLive = 0x497DAC;
-    DWORD g_pLocalPlayer = 0x497E04;
-    DWORD clockMin = 0x497F10;
-    DWORD specFollow = 0x497FA4;
-    DWORD specBase = 0x498364;
-    DWORD camera_x = 0x498D4C;
-    DWORD patchNoFlash = 0x2A3441;
-    DWORD patchNoFow = 0x27CBF0;
+    DWORD g_pPlayersEntityList = 0x49A390;
+    DWORD screen_width = 0x4AAAD0;
+    DWORD cursor_x = 0x4AAD44;
+    DWORD isMatchLive = 0x4AAE3C;
+    DWORD g_pLocalPlayer = 0x4AAE94;
+    DWORD clockMin = 0x4AAFA0;
+    DWORD specFollow = 0x4AB034;
+    DWORD specBase = 0x4AB3F8;
+    DWORD camera_x = 0x4ABDDC;
+    DWORD patchNoFlash = 0x2A6E56;
+    DWORD patchNoFow = 0x27F0F3 - 0x1;
 
     DWORD cursor_y = cursor_x + 0x4;
     DWORD screen_height = screen_width + 0x4;
